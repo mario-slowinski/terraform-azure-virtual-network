2e586d99604a207f3505786e9dd08aeb6bb87cda
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-03-12 16:47:21 +0100

    Add private_dns_zone_link.

---
+++ private_dns_zone_link.tf
@@ -0,0 +1,14 @@
+resource "azurerm_private_dns_zone_virtual_network_link" "many" {
+  count = length(var.private_dns_zones)
+
+  name                  = coalesce(var.private_dns_zones[count.index].name, var.name)
+  private_dns_zone_name = var.private_dns_zones[count.index].private_dns_zone_name
+  resource_group_name   = var.resource_group_name
+  virtual_network_id    = azurerm_virtual_network.this.id
+  registration_enabled  = try(var.private_dns_zones[count.index].registration_enabled, null)
+  tags = coalesce(
+    var.private_dns_zones[count.index].tags,
+    var.tags,
+    local.zip
+  )
+}
---
+++ private_dns_zone_link.vars.tf
@@ -0,0 +1,10 @@
+variable "private_dns_zones" {
+  type = list(object({
+    name                  = optional(string)
+    private_dns_zone_name = string
+    registration_enabled  = optional(bool)
+    tags                  = optional(map(string))
+  }))
+  description = "List of private DNS Zones to link to this virtual network."
+  default     = []
+}
