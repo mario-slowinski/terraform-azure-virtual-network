cb7c102e0b2a4fe21e165ce2a480e0f156ad50c3
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-03-12 23:28:02 +0100

    Update "local.tags".

--- locals.tf
+++ locals.tf
@@ -1,13 +1,10 @@
 locals {
-  zip = zipmap(var.tags_keys, var.tags_values)
-
   # must be as separated lists because maps are automatically sorted
-  tags = [
-    for key, value in local.zip : {
-      key   = key
-      value = value
-    } if value != ""
-  ]
+  tags = {
+    for key, value in zipmap(var.tags_keys, var.tags_values) :
+    key => value
+    if value != ""
+  }
 
   names = compact(matchkeys(var.tags_values, var.tags_keys, var.names_keys))
 
