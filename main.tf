7e7b45fc4c17f748d96fca09785de8cd7c30d844
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-03-19 10:14:52 +0100

    Support additional arguments.

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
 resource "azurerm_virtual_network" "this" {
   name                = coalesce(var.name, local.name)
   resource_group_name = coalesce(var.resource_group_name, local.name)
-  location            = var.location
   address_space       = var.address_space
+  location            = var.location
+
+  dns_servers             = var.dns_servers
+  edge_zone               = var.edge_zone
+  flow_timeout_in_minutes = var.flow_timeout_in_minutes
+
+  dynamic "ddos_protection_plan" {
+    for_each = length(var.ddos_protection_plan.id) > 0 ? toset([1]) : toset([])
+
+    content {
+      id     = var.ddos_protection_plan.id
+      enable = var.ddos_protection_plan.enable
+    }
+  }
 
   tags = coalesce(var.tags, local.tags)
 }
--- virtual_network.vars.tf
+++ virtual_network.vars.tf
@@ -21,3 +21,33 @@ variable "location" {
   description = "Region to create resource."
   default     = ""
 }
+
+variable "ddos_protection_plan" {
+  type = object({
+    id     = string
+    enable = bool
+  })
+  description = "Optional block."
+  default = {
+    id     = ""
+    enable = false
+  }
+}
+
+variable "dns_servers" {
+  type        = list(string)
+  description = "List of IP addresses of DNS servers."
+  default     = null
+}
+
+variable "edge_zone" {
+  type        = string
+  description = "Specifies the Edge Zone within the Azure Region where this Virtual Network should exist."
+  default     = null
+}
+
+variable "flow_timeout_in_minutes" {
+  type        = number
+  description = "The flow timeout in minutes for the Virtual Network, which is used to enable connection tracking for intra-VM flows."
+  default     = null
+}
