cb1856546f7e9b441b3cdf354d7230ec96e84b55
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-19 11:18:35 +0200

    Replace "public_cert_data" with "thumbprint" in "revoked_certificate" block.

--- gateway.main.tf
+++ gateway.main.tf
@@ -89,8 +89,8 @@ resource "azurerm_virtual_network_gateway" "name" {
           if revoked_certificate.name != null
         }
         content {
-          name             = root_certificate.value.name
-          public_cert_data = root_certificate.value.public_cert_data
+          name       = root_certificate.value.name
+          thumbprint = root_certificate.value.thumbprint
         }
       }
 
