7403496baa3d5b1caedb1c4e0339342495e027ad
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-27 19:16:27 +0200

    Change "count" to "for_each" in "private_dns_zone_link".

--- private_dns_zone_link.main.tf
+++ private_dns_zone_link.main.tf
@@ -1,16 +1,17 @@
-resource "azurerm_private_dns_zone_virtual_network_link" "many" {
-  count = length(var.private_dns_zones)
+resource "azurerm_private_dns_zone_virtual_network_link" "zone" {
+  for_each = {
+    for private_dns_zone in var.private_dns_zones :
+    private_dns_zone.private_dns_zone_name => private_dns_zone
+    if private_dns_zone.private_dns_zone_name != null
+  }
 
-  name                  = coalesce(var.private_dns_zones[count.index].name, var.name)
-  private_dns_zone_name = var.private_dns_zones[count.index].private_dns_zone_name
-  resource_group_name   = coalesce(var.private_dns_zones[count.index].resource_group_name, var.resource_group_name)
+  name                  = coalesce(each.value.name, local.name)
+  private_dns_zone_name = each.value.private_dns_zone_name
+  resource_group_name   = coalesce(each.value.resource_group_name, var.resource_group_name)
   virtual_network_id    = azurerm_virtual_network.this.id
-  registration_enabled  = try(var.private_dns_zones[count.index].registration_enabled, null)
-  tags = coalesce(
-    var.private_dns_zones[count.index].tags,
-    var.tags,
-    local.tags
-  )
+  registration_enabled  = each.value.registration_enabled
+  tags                  = merge(local.tags, var.tags, each.value.tags)
+
   depends_on = [
     azurerm_virtual_network.this,
   ]
--- private_dns_zone_link.vars.tf
+++ private_dns_zone_link.vars.tf
@@ -1,11 +1,11 @@
 variable "private_dns_zones" {
   type = list(object({
-    name                  = optional(string)
-    private_dns_zone_name = string
-    resource_group_name   = optional(string)
-    registration_enabled  = optional(bool)
+    name                  = optional(string) # The name of the Private DNS Zone Virtual Network Link.
+    private_dns_zone_name = string           # The name of the Private DNS zone (without a terminating dot).
+    resource_group_name   = optional(string) # Specifies the resource group where the Private DNS Zone exists.
+    registration_enabled  = optional(bool)   # Is auto-registration of virtual machine records in the virtual network in the Private DNS zone enabled?
     tags                  = optional(map(string))
   }))
   description = "List of private DNS Zones to link to this virtual network."
-  default     = []
+  default     = [{ private_dns_zone_name = null }]
 }
