d27b703d54b0c753b5abd46caff4f06aa0d7b030
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-27 18:03:26 +0200

    Get data of remote virtual network in peering.

---
+++ peering.data.tf
@@ -0,0 +1,10 @@
+data "azurerm_virtual_network" "remote" {
+  for_each = {
+    for peering in var.peerings :
+    try(peering.local.name, peering.remote.virtual_network_name) => peering
+    if peering.remote.virtual_network_name != null
+  }
+
+  name                = each.value.remote.virtual_network_name
+  resource_group_name = each.value.remote.resource_group_name
+}
