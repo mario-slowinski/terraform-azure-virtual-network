79464ec8d39c9f4a2970877ae9788f8c3845f1e0
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-27 18:03:47 +0200

    Add "local" and "remote" peering.

--- peering.main.tf
+++ peering.main.tf
@@ -1,17 +1,35 @@
-resource "azurerm_virtual_network_peering" "name" {
+resource "azurerm_virtual_network_peering" "remote" {
   for_each = {
     for peering in var.peerings :
-    peering.name => peering
-    if peering.name != null
+    coalesce(peering.remote.name, var.name, local.name) => peering
+    if peering.remote.virtual_network_name != null
   }
 
-  name                         = coalesce(each.value.name, var.name, local.name)
+  name                         = each.key
+  virtual_network_name         = each.value.remote.virtual_network_name
+  remote_virtual_network_id    = azurerm_virtual_network.this.id
+  resource_group_name          = each.value.remote.resource_group_name
+  allow_virtual_network_access = each.value.remote.allow_virtual_network_access
+  allow_forwarded_traffic      = each.value.remote.allow_forwarded_traffic
+  allow_gateway_transit        = each.value.remote.allow_gateway_transit
+  use_remote_gateways          = each.value.remote.use_remote_gateways
+  triggers                     = each.value.remote.triggers
+}
+
+resource "azurerm_virtual_network_peering" "local" {
+  for_each = {
+    for peering in var.peerings :
+    try(peering.local.name, peering.remote.virtual_network_name) => peering
+    if peering.remote.virtual_network_name != null
+  }
+
+  name                         = each.key
   virtual_network_name         = azurerm_virtual_network.this.name
-  remote_virtual_network_id    = each.value.remote_virtual_network_id
-  resource_group_name          = coalesce(each.value.resource_group_name, var.resource_group_name, local.name)
-  allow_virtual_network_access = each.value.allow_virtual_network_access
-  allow_forwarded_traffic      = each.value.allow_forwarded_traffic
-  allow_gateway_transit        = each.value.allow_gateway_transit
-  use_remote_gateways          = each.value.use_remote_gateways
-  triggers                     = each.value.triggers
+  remote_virtual_network_id    = data.azurerm_virtual_network.remote[each.key].id
+  resource_group_name          = try(each.value.local.resource_group_name, var.resource_group_name)
+  allow_virtual_network_access = try(each.value.local.allow_virtual_network_access, null)
+  allow_forwarded_traffic      = try(each.value.local.allow_forwarded_traffic, null)
+  allow_gateway_transit        = try(each.value.local.allow_gateway_transit, null)
+  use_remote_gateways          = try(each.value.local.use_remote_gateways, null)
+  triggers                     = try(each.value.local.triggers, null)
 }
